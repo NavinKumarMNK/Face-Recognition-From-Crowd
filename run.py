323f877aa3d36caacbfad9cd0994672ebefe4822
NavinKumarMNK <mnk.navin@outlook.com>
2023-01-10 06:45:16 +0530

    full architecture with errors

--- run.py
+++ run.py
@@ -2,8 +2,23 @@ import torch
 import pytorch_lightning as pt
 from pytorch_lightning import Trainer
 import argparse
+import asyncio
+from scripts.process import Process
+
+
+class Main():
+    def __init__(self, args) -> None:
+        self.args = args
+
+    def run(self):
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        Process(
+            './temp', './weights/yolov7-tinyface.pt', device).set_capture(self.args.source).start_capture()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Face Recognition")
-    parser.parse_args()
+    parser.add_argument('--source', type=str,
+                        default="video", help="stream/video/image")
+    args = parser.parse_args()
+    Main(args).run()
---
+++ scripts/facenet.py
@@ -0,0 +1,10 @@
+class FaceNet():
+    def __init__(self) -> None:
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    FaceNet().run()
